f8f503a2837dc309619e9ed1ea4c4df6041a56b3
王金东(17082054) <17082054@suning.com>
2020-07-11 17:18:31 +0800

    add memeber home

---
+++ flutter_component/lib/page/my/jd_member_home_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+/// @author jd
+
+class JDMemberHomePage extends StatefulWidget {
+  @override
+  _JDMemberHomePageState createState() => _JDMemberHomePageState();
+}
+
+class _JDMemberHomePageState extends State<JDMemberHomePage> {
+  Widget _buildBackHomeList() {
+    return ListView.separated(
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(title: Text('个人信息 $index'));
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider();
+        },
+        itemCount: 100);
+  }
+
+  Widget _buildBackHome() {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 200,
+          color: Colors.red,
+          alignment: Alignment.center,
+          child: const Text('个人介绍'),
+        ),
+        Expanded(
+          child: _buildBackHomeList(),
+        )
+      ],
+    );
+  }
+
+  Widget _buildDragList(ScrollController scrollController) {
+    return ListView.builder(
+      controller: scrollController,
+      itemCount: 100,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(title: Text('评论 $index'));
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('个人主页'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: double.infinity,
+            alignment: Alignment.topLeft,
+            child: FractionallySizedBox(
+              heightFactor: .8,
+              child: _buildBackHome(),
+            ),
+          ),
+          DraggableScrollableSheet(
+            expand: true,
+            initialChildSize: 0.2,
+            minChildSize: 0.2,
+            maxChildSize: 0.8,
+            builder: (BuildContext context, ScrollController scrollController) {
+              return Container(
+                color: Colors.blue[100],
+                child: _buildDragList(scrollController),
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
--- flutter_component/lib/page/my/jd_my_page.dart
+++ flutter_component/lib/page/my/jd_my_page.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_component/page/setting/jd_setting_page.dart';
 import 'package:flutter_component/utils/jd_asset_bundle.dart';
 import 'package:flutter_component/utils/jd_navigation_util.dart';
-import 'package:flutter_component/widget/blurRect/jd_blurrect.dart';
+
+import 'jd_member_home_page.dart';
 
 /**
  *
@@ -10,92 +11,50 @@ import 'package:flutter_component/widget/blurRect/jd_blurrect.dart';
  *
  */
 
-mixin _CounterStateMixin < T extends StatefulWidget> on State<T> {
+mixin _CounterStateMixin<T extends StatefulWidget> on State<T> {
   int _counter = 0;
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
   }
-
 }
 
 class JDMyPage extends StatefulWidget {
-
   final String title = "我的";
 
   @override
   State createState() => _JDMyPageState();
 }
 
-class _JDMyPageState extends State<JDMyPage> with _CounterStateMixin,AutomaticKeepAliveClientMixin {
-
+class _JDMyPageState extends State<JDMyPage>
+    with _CounterStateMixin, AutomaticKeepAliveClientMixin {
   int _appBarStyle = 0;
   bool _showToTopBtn = false; //是否显示“返回到顶部”按钮
   ScrollController _controller = new ScrollController();
 
   final _headMenu = [
+    {'icon': Icons.favorite, 'title': "我的收藏"},
+    {'icon': Icons.center_focus_strong, 'title': "我的关注"},
     {
-      'icon' : Icons.favorite,
-      'title' : "我的收藏"
+      'icon': Icons.border_horizontal,
+      'title': "足迹",
     },
-    {
-      'icon' : Icons.center_focus_strong,
-      'title' : "我的关注"
-    },
-    {
-      'icon' : Icons.border_horizontal,
-      'title' : "足迹",
-    },
-    {
-      'icon' : Icons.contact_phone,
-      'title' : "优惠券"
-    }
+    {'icon': Icons.contact_phone, 'title': "优惠券"}
   ];
 
   var gridMenu = [
-    {
-      'icon' : Icons.assignment,
-      'title' : '发票'
-    },
-    {
-      'icon' : Icons.contact_phone,
-      'title' : '客服'
-    },
-    {
-      'icon' : Icons.border_color,
-      'title' : '意见反馈'
-    },
-    {
-      'icon' : Icons.help,
-      'title' : '帮助'
-    },
-    {
-      'icon' : Icons.room_service,
-      'title' : '服务估价'
-    },
-    {
-      'icon' : Icons.people,
-      'title' : '合作伙伴'
-    },
-    {
-      'icon' : Icons.color_lens,
-      'title' : '我的余额'
-    },
-    {
-      'icon' : Icons.search,
-      'title' : '免费核名'
-    },
-    {
-      'icon' : Icons.class_,
-      'title' : '分类'
-    },
-    {
-      'icon' : Icons.search,
-      'title' : '查询'
-    },
+    {'icon': Icons.assignment, 'title': '发票'},
+    {'icon': Icons.contact_phone, 'title': '客服'},
+    {'icon': Icons.border_color, 'title': '意见反馈'},
+    {'icon': Icons.help, 'title': '帮助'},
+    {'icon': Icons.room_service, 'title': '服务估价'},
+    {'icon': Icons.people, 'title': '合作伙伴'},
+    {'icon': Icons.color_lens, 'title': '我的余额'},
+    {'icon': Icons.search, 'title': '免费核名'},
+    {'icon': Icons.class_, 'title': '分类'},
+    {'icon': Icons.search, 'title': '查询'},
   ];
-
 
   @override
   void initState() {
@@ -114,10 +73,20 @@ class _JDMyPageState extends State<JDMyPage> with _CounterStateMixin,AutomaticKe
   Widget _buildStyle0AppBar() {
     return SliverAppBar(
       expandedHeight: 250,
-      centerTitle:true,
+      centerTitle: true,
       leading: _buildLeading(),
       actions: _buildAction(),
-      title: const Text('我的',style: TextStyle(color: Colors.white),),
+      title: InkWell(
+        onTap: () {
+          setState(() {
+            _appBarStyle = (_appBarStyle == 0 ? 1 : 0);
+          });
+        },
+        child: const Text(
+          '我的',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
       elevation: 2,
       pinned: true,
       backgroundColor: Colors.orange,
@@ -133,12 +102,10 @@ class _JDMyPageState extends State<JDMyPage> with _CounterStateMixin,AutomaticKe
               width: double.infinity,
               fit: BoxFit.fitWidth,
             ),
-
             _buildMesnuGrid(),
           ],
         ),
       ),
-
     );
   }
 
@@ -149,29 +116,46 @@ class _JDMyPageState extends State<JDMyPage> with _CounterStateMixin,AutomaticKe
       leading: _buildLeading(),
       actions: _buildAction(),
       flexibleSpace: FlexibleSpaceBar(
-        title: const Text('我的',style: TextStyle(color: Colors.white),),
-        background: Image.asset(JDAssetBundle.getImgPath("user_head"), fit: BoxFit.cover,),
+        title: InkWell(
+          onTap: () {
+            setState(() {
+              _appBarStyle = (_appBarStyle == 0 ? 1 : 0);
+            });
+          },
+          child: const Text(
+            '我的',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        background: Image.asset(
+          JDAssetBundle.getImgPath("user_head"),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
 
   Widget _buildLeading() => Container(
-    margin: EdgeInsets.all(10),
-    child: InkWell(
-      onTap: (){
-        setState(() {
-          _appBarStyle = (_appBarStyle == 0 ? 1 : 0) ;
-        });
-      },
-      child: Image.asset(JDAssetBundle.getImgPath("head")),
-    ),
-  );
+        margin: const EdgeInsets.all(10),
+        child: InkWell(
+          onTap: () {
+            JDNavigationUtil.push(JDMemberHomePage());
+          },
+          child: Image.asset(JDAssetBundle.getImgPath("head")),
+        ),
+      );
 
   List<Widget> _buildAction() => <Widget>[
-    IconButton(icon: Icon(Icons.settings,color: _appBarStyle == 0 ? Colors.white : Colors.black12 ,),onPressed: () {
-      JDNavigationUtil.push(JDSettingPage());
-    },)
-  ];
+        IconButton(
+          icon: Icon(
+            Icons.settings,
+            color: _appBarStyle == 0 ? Colors.white : Colors.black12,
+          ),
+          onPressed: () {
+            JDNavigationUtil.push(JDSettingPage());
+          },
+        )
+      ];
 
   Widget _buildMesnuGrid() {
     return Container(
@@ -181,21 +165,27 @@ class _JDMyPageState extends State<JDMyPage> with _CounterStateMixin,AutomaticKe
         shrinkWrap: true,
         padding: const EdgeInsets.all(0),
         physics: const NeverScrollableScrollPhysics(),
-        children: _headMenu.map((e) => Container(
-          color: Colors.transparent,
-          child: InkWell(
-            onTap: () {
-            },
-            child: Tab(
-              icon: Icon(e['icon'] as IconData,color: Colors.white,),
-              child: Text(e['title'].toString(),style: const TextStyle(color: Colors.white),),
-            ),
-          ),
-        )).toList(),
+        children: _headMenu
+            .map((e) => Container(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Tab(
+                      icon: Icon(
+                        e['icon'] as IconData,
+                        color: Colors.white,
+                      ),
+                      child: Text(
+                        e['title'].toString(),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ))
+            .toList(),
       ),
     );
   }
-
 
   Widget _buildOrder() {
     return SliverToBoxAdapter(
@@ -207,11 +197,11 @@ class _JDMyPageState extends State<JDMyPage> with _CounterStateMixin,AutomaticKe
               children: <Widget>[
                 Container(
                   padding: const EdgeInsets.only(left: 10),
-                  child: Text('我的订单',
-                    style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                  child: Text(
+                    '我的订单',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                 ),
-
                 Container(
                   child: FlatButton(
                     child: Row(
@@ -220,10 +210,9 @@ class _JDMyPageState extends State<JDMyPage> with _CounterStateMixin,AutomaticKe
                         Icon(Icons.keyboard_arrow_right),
                       ],
                     ),
-                    onPressed: (){},
+                    onPressed: () {},
                   ),
                 ),
-
               ],
             ),
             const Divider(),
@@ -231,13 +220,20 @@ class _JDMyPageState extends State<JDMyPage> with _CounterStateMixin,AutomaticKe
               leading: Image.asset(JDAssetBundle.getImgPath("head")),
               title: Container(
                 padding: const EdgeInsets.only(bottom: 5),
-                child: Text('发布需求',style: TextStyle(fontSize: 14,color: Colors.black),),
+                child: Text(
+                  '发布需求',
+                  style: TextStyle(fontSize: 14, color: Colors.black),
+                ),
               ),
-              subtitle: Text('免费获取方案和报价',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16,color: Colors.black54),),
+              subtitle: Text(
+                '免费获取方案和报价',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    color: Colors.black54),
+              ),
               trailing: OutlineButton(
-                onPressed: () {
-
-                },
+                onPressed: () {},
                 child: const Text('立即获取'),
                 shape: const RoundedRectangleBorder(
                   borderRadius: BorderRadius.all(Radius.circular(20)),
@@ -250,7 +246,7 @@ class _JDMyPageState extends State<JDMyPage> with _CounterStateMixin,AutomaticKe
     );
   }
 
- Widget _buildMenu() {
+  Widget _buildMenu() {
     return SliverToBoxAdapter(
       child: Card(
         child: Column(
@@ -262,22 +258,22 @@ class _JDMyPageState extends State<JDMyPage> with _CounterStateMixin,AutomaticKe
                   height: 50,
                   alignment: Alignment.center,
                   padding: EdgeInsets.only(left: 10),
-                  child: Text('常用功能',
-                    style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                  child: Text(
+                    '常用功能',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                 ),
-
               ],
             ),
             Divider(),
             GridView.count(
               crossAxisCount: 4,
-              physics:NeverScrollableScrollPhysics(),
+              physics: NeverScrollableScrollPhysics(),
               crossAxisSpacing: 10,
               mainAxisSpacing: 10,
               padding: EdgeInsets.all(0),
               childAspectRatio: 1,
-              shrinkWrap: true,//解决gridview不能在customScrollView显示的问题
+              shrinkWrap: true, //解决gridview不能在customScrollView显示的问题
               children: gridMenu.map((item) => _getItem(item)).toList(),
             )
           ],
@@ -286,11 +282,9 @@ class _JDMyPageState extends State<JDMyPage> with _CounterStateMixin,AutomaticKe
     );
   }
 
-  Widget _getItem(Map<String,dynamic> item) {
+  Widget _getItem(Map<String, dynamic> item) {
     return InkWell(
-      onTap: () {
-
-      },
+      onTap: () {},
       child: Tab(
         icon: Icon(item['icon'] as IconData),
         text: item['title'].toString(),
@@ -304,7 +298,7 @@ class _JDMyPageState extends State<JDMyPage> with _CounterStateMixin,AutomaticKe
         alignment: Alignment.center,
         child: Text(
           '--- 为您推荐-人气店铺 ---',
-          style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
       ),
     );
@@ -313,32 +307,30 @@ class _JDMyPageState extends State<JDMyPage> with _CounterStateMixin,AutomaticKe
   Widget _buildList() {
     return SliverFixedExtentList(
       itemExtent: 80.0,
-      delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-            //创建列表项
-            return Container(
-              margin: const EdgeInsets.symmetric(horizontal: 10),
-              color: Colors.white,
-              child: InkWell(
-                onTap: () {
-
-                },
-                child:  Column(
-                  children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: Text('list item $index'),
-                    ),
+      delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+        //创建列表项
+        return Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10),
+          color: Colors.white,
+          child: InkWell(
+            onTap: () {},
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Text('list item $index'),
                   ),
-                   const Divider(height: 1,),
-                  ],
                 ),
-              ),
-            );
-          },
-          childCount: 30 //50个列表项
-      ),
+                const Divider(
+                  height: 1,
+                ),
+              ],
+            ),
+          ),
+        );
+      }, childCount: 30 //50个列表项
+          ),
     );
   }
 
@@ -358,47 +350,45 @@ class _JDMyPageState extends State<JDMyPage> with _CounterStateMixin,AutomaticKe
   Widget build(BuildContext context) {
     return Material(
       child: NotificationListener(
-        onNotification: (Notification scrollNotification) {
-          if ((scrollNotification is ScrollUpdateNotification) && (scrollNotification.depth == 0)) {
-            _onScroll(scrollNotification.metrics.pixels);
-          }
-          return true;
-        },
-        child: Stack(
-          children: <Widget>[
-            Container(
-              color: Colors.grey[200],
-            ),
+          onNotification: (Notification scrollNotification) {
+            if ((scrollNotification is ScrollUpdateNotification) &&
+                (scrollNotification.depth == 0)) {
+              _onScroll(scrollNotification.metrics.pixels);
+            }
+            return true;
+          },
+          child: Stack(
+            children: <Widget>[
+              Container(
+                color: Colors.grey[200],
+              ),
+              CustomScrollView(
+                controller: _controller,
+                slivers: <Widget>[
+                  //AppBar，包含一个导航栏
+                  _appBarStyle == 0
+                      ? _buildStyle0AppBar()
+                      : _buildStyle1AppBar(),
 
-            CustomScrollView(
-              controller: _controller,
-              slivers: <Widget>[
-                //AppBar，包含一个导航栏
-                _appBarStyle == 0 ? _buildStyle0AppBar():_buildStyle1AppBar(),
+                  SliverPadding(
+                    padding: const EdgeInsets.all(10),
+                    sliver: _buildOrder(),
+                  ),
 
-                SliverPadding(
-                  padding: const EdgeInsets.all(10),
-                  sliver: _buildOrder(),
-                ),
+                  SliverPadding(
+                    padding: const EdgeInsets.all(10),
+                    sliver: _buildMenu(),
+                  ),
 
-                SliverPadding(
-                  padding: const EdgeInsets.all(10),
-                  sliver: _buildMenu(),
-                ),
-
-                SliverPadding(
-                  padding: const EdgeInsets.all(10),
-                  sliver: _buildRecommend(),
-                ),
-                _buildList(),
-              ],
-            ),
-
-          ],
-        )
-
-
-      ),
+                  SliverPadding(
+                    padding: const EdgeInsets.all(10),
+                    sliver: _buildRecommend(),
+                  ),
+                  _buildList(),
+                ],
+              ),
+            ],
+          )),
     );
   }
 
